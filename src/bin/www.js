42b35b6f5478c56bd788e70cfde9d33a259e38a3
crno <118383162+Crn0@users.noreply.github.com.>
2025-03-13 14:17:19 +0800

    refactor: remove process events and change server script

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,20 +1,13 @@
 import "dotenv/config";
 import http from "http";
-import path, { dirname } from "path";
-import debugFunc from "debug";
-import { fileURLToPath } from "url";
+import Debug from "debug";
 import app from "../app.js";
 import constants from "../constants/index.js";
-import cleanup from "../helpers/cleanups/index.js";
 
-// eslint-disable-next-line no-underscore-dangle
-const __dirname =
-  import.meta.dirname || dirname(fileURLToPath(import.meta.url));
-const dirnameArrays = path.resolve(__dirname, "..", "..").split(path.sep);
-const debug = debugFunc(`${dirnameArrays[dirnameArrays.length - 1]}:server`);
 const port = constants.env.PORT;
+const debug = constants.env.NODE_ENV === "dev" ? Debug("app:server") : () => {};
 
-function onError(error) {
+const onError = (error) => {
   if (error.syscall !== "listen") {
     throw error;
   }
@@ -34,16 +27,13 @@ function onError(error) {
     default:
       throw error;
   }
-}
+};
 
-function onListening(server) {
-  return () => {
-    const addr = server.address();
-    const bind =
-      typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
-    debug(`Listening on ${bind}`);
-  };
-}
+const onListening = (server) => () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+};
 
 app.set("port", port);
 
@@ -52,26 +42,3 @@ const server = http.createServer(app);
 server.listen(port);
 server.on("error", onError);
 server.on("listening", onListening(server));
-
-process.stdin.resume();
-/**
- *  remove the uploaded image when the server closed
- */
-[
-  `exit`,
-  `SIGINT`,
-  `SIGUSR1`,
-  `SIGUSR2`,
-  `uncaughtException`,
-  `SIGTERM`,
-].forEach((eventType) => {
-  process.on(eventType, async () => {
-    try {
-      await cleanup.images(__dirname);
-      process.exit(0);
-    } catch (error) {
-      console.error(error);
-      process.exit(1);
-    }
-  });
-});
