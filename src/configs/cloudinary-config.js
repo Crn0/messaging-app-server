5535173f322a79ed1331acd094185842979d8339
crno <118383162+Crn0@users.noreply.github.com.>
2025-03-27 18:17:39 +0800

    refactor configs

--- src/configs/cloudinary-config.js
+++ src/configs/cloudinary-config.js
@@ -1,10 +1,9 @@
-import { v2 as cloudinary } from "cloudinary";
 import constants from "../constants/index.js";
 
-cloudinary.config({
+const config = {
   cloud_name: constants.env.CLOUDINARY_NAME,
   api_key: constants.env.CLOUDINARY_API_KEY,
   api_secret: constants.env.CLOUDINARY_SECRET,
-});
+};
 
-export default cloudinary;
+export default config;
--- src/configs/multer-config.js
+++ src/configs/multer-config.js
@@ -1,24 +1,39 @@
-import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
 import * as crypto from "node:crypto";
+import { fileExtension } from "../helpers/index.js";
+import FieldError from "../errors/field-error.js";
 
-const fileExtensions = (mimeType) => {
-  switch (mimeType) {
-    case "image/png":
-      return ".png";
-    case "image/jpeg":
-      return ".jpeg";
-    case "image/jpg":
-      return ".jpeg";
-    case "image/webp":
-      return ".webp";
-    default:
-      return "";
+const MAX_FILE_SIZE = 10_000_000; // 10mb
+
+function fileFilter(req, file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|epub|pdf/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
   }
-};
 
-const storage = multer.diskStorage({
+  return cb(
+    new FieldError(
+      "Validation Failed",
+      {
+        type: "fields",
+        field: `${file.fieldname}`,
+        message: `400 Bad Request: The MIME type ${file.mimetype} is not supported by the server. Please upload a file with one of the following supported formats: .jpg, .png, .webp, .pdf or .epub`,
+      },
+      400
+    ),
+
+    false
+  );
+}
+
+const storage = {
   destination: (req, file, cb) => {
     cb(
       null,
@@ -26,16 +41,16 @@ const storage = multer.diskStorage({
         import.meta.dirname || path.dirname(fileURLToPath(import.meta.url)),
         "..",
         "temp",
-        "images",
-      ),
+        "images"
+      )
     );
   },
   filename: (req, file, cb) => {
-    const name = `${file.fieldname}-${crypto.randomBytes(10).toString("hex")}${fileExtensions(file.mimetype)}`;
+    const name = `${file.fieldname}-${crypto.randomBytes(10).toString("hex")}${fileExtension(file.mimetype)}`;
     cb(null, name);
   },
-});
+};
 
-const upload = multer({ storage });
+const config = { storage, fileFilter, MAX_FILE_SIZE };
 
-export default upload;
+export default config;
