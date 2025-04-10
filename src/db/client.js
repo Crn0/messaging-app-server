d385652994e9010c559e8ca843b96bb4bf73e334
crno <118383162+Crn0@users.noreply.github.com.>
2025-04-10 23:44:52 +0800

    build: use enviroment variable DATABASE_URL

--- src/db/client.js
+++ src/db/client.js
@@ -1,10 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-import constants from "../constants/index.js";
+import { env } from "../constants/index.js";
 
-const databaseUrl =
-  constants.env.NODE_ENV === "test"
-    ? constants.env.TEST_DATABASE_URL
-    : constants.env.DATABASE_URL;
+const databaseUrl = env.DATABASE_URL;
 
 export default new PrismaClient({
   datasources: {
