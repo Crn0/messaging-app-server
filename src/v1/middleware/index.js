1b580ab05e23269293e5c4a17d284ca75d39e793
crno <118383162+Crn0@users.noreply.github.com.>
2025-04-10 23:46:14 +0800

    build: create zod middleware

---
+++ src/v1/middleware/index.js
@@ -0,0 +1,13 @@
+import {
+  ZodbodyValidator,
+  ZodqueryValidator,
+  ZodparamValidator,
+  ZodfileValidator,
+} from "./validator.js";
+
+export {
+  ZodbodyValidator,
+  ZodqueryValidator,
+  ZodparamValidator,
+  ZodfileValidator,
+};
---
+++ src/v1/validator/zod-body-validator.js
@@ -0,0 +1,24 @@
+import { ZodError } from "zod";
+import ValidationError from "../../errors/validation-error.js";
+
+const ZodbodyValidator = (schema) => (req, res, next) => {
+  try {
+    req.body = schema.parse(req.body);
+
+    next();
+  } catch (e) {
+    let message = "Validation Failed";
+    let errors = {};
+
+    if (e instanceof ZodError) {
+      message = `Validation failed: ${e.issues.length} errors detected in body`;
+      errors = e.issues;
+    }
+
+    const unprocessableRequestError = new ValidationError(message, errors);
+
+    next(unprocessableRequestError);
+  }
+};
+
+export default ZodbodyValidator;
