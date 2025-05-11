d39a3ff1d533282c6b0f0b2236cc1aab78c5cea0
crno <118383162+Crn0@users.noreply.github.com.>
2025-05-11 14:38:53 +0800

    build(app): handle parse error and not trusted error

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import helmet from "helmet";
 import logger from "morgan";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
+import { httpStatus } from "./constants/index.js";
 import configs from "./configs/index.js";
 import ErrorHandler from "./errors/error-handler.js";
 
@@ -29,11 +30,18 @@ app.use(express.static(join(__dirname, "..", "public")));
 
 // error handler
 app.use((err, req, res, _) => {
-  if (!ErrorHandler.isTrustedError(err)) {
-    process.exit(1);
-  } else {
-    ErrorHandler.handleError(err, res);
+  if (err.type === "entity.parse.failed") {
+    return res.status(err.status).json({
+      code: err.statusCode,
+      message: err.message,
+    });
   }
+
+  if (!ErrorHandler.isTrustedError(err)) {
+    return res.sendStatus(httpStatus.INTERNAL_SERVER);
+  }
+
+  return ErrorHandler.handleError(err, res);
 });
 
 export default app;
