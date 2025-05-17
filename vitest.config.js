005a5cc59f8a879f86427f3090c34d4a7f2b979c
crno <118383162+Crn0@users.noreply.github.com.>
2025-05-17 15:51:04 +0800

    build(vite): global setup to clean the database

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    globalSetup: "test/helpers/global-teardown.js",
     include: ["src/**/*.test.js"],
     clearMocks: true,
     threads: true,
