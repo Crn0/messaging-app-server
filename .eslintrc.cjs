514fa0eb1b82200544865d17a4a186e4f4528b3c
crno <118383162+Crn0@users.noreply.github.com.>
2025-05-18 17:47:31 +0800

    refactor(eslint): remove test files in the ignore patterns

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   env: { browser: false, es2020: true, jest: true },
   extends: ["eslint:recommended", "airbnb", "prettier"],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "**.test.js"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   rules: {
