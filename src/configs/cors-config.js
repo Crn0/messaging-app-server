50fc5c0d4f684374f0614ec2d08bcb9f66a0a76d
crno <118383162+Crn0@users.noreply.github.com.>
2025-07-02 17:16:46 +0800

    feat(configs/cors): set credentials to true

--- src/configs/cors-config.js
+++ src/configs/cors-config.js
@@ -3,6 +3,7 @@ import { env } from "../constants/index.js";
 const corsOptions = {
   origin: env.CORS_ORIGINS,
   methods: env.CORS_METHODS,
+  credentials: true,
   optionsSuccessStatus: 200,
 };
 
