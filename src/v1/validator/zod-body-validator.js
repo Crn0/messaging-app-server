9b5c1a859050a1637252c82e76ed9a0513627c4f
crno <118383162+Crn0@users.noreply.github.com.>
2025-08-03 17:13:30 +0800

    refactor(v1/validator): clean up uploaded files on body validation error

--- src/v1/validator/zod-body-validator.js
+++ src/v1/validator/zod-body-validator.js
@@ -1,7 +1,41 @@
 import { ZodError } from "zod";
+import { unlink } from "fs/promises";
+
 import ValidationError from "../../errors/validation-error.js";
 
-const ZodbodyValidator = (schema) => (req, res, next) => {
+const cleanFiles = async (req) => {
+  const unlinkPromises = [];
+
+  if (req.files && typeof req.files === "object" && !Array.isArray(req.files)) {
+    const entries = Object.entries(req.files);
+
+    entries.forEach(([_, fileOrFiles]) => {
+      const files = Array.isArray(fileOrFiles) ? fileOrFiles : [fileOrFiles];
+
+      files.forEach((file) => {
+        if (file?.path) unlinkPromises.push(unlink(file.path));
+      });
+    });
+  }
+
+  if (Array.isArray(req.body?.attachments)) {
+    req.body?.attachments.forEach((file) =>
+      unlinkPromises.push(unlink(file.path))
+    );
+  }
+
+  if (req.body?.avatar || req.body?.backgroundAvatar) {
+    const files = [req.body?.avatar, req.body?.backgroundAvatar].filter(
+      Boolean
+    );
+
+    files.forEach((file) => unlinkPromises.push(unlink(file.path)));
+  }
+
+  await Promise.allSettled(unlinkPromises);
+};
+
+const ZodbodyValidator = (schema) => async (req, res, next) => {
   if (req.file) {
     req.body.avatar = req.file;
   }
@@ -32,6 +66,8 @@ const ZodbodyValidator = (schema) => (req, res, next) => {
   } catch (e) {
     let message = "Validation Failed";
     let errors = {};
+
+    await cleanFiles(req);
 
     if (e instanceof ZodError) {
       message = `Validation failed: ${e.issues.length} errors detected in body`;
